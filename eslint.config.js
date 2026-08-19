053155afd4c2a4ea353d274f2780215463d035e5
haeun <sunghaeun1004@gmail.com>
2026-08-19 13:45:40 +0900

    feat : pdf 추출 오류 수정

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
+      js.configs.recommended, 
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
